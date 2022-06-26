f7058bfd0d9b10f09b2370033af6c32ae51a0271
Alexei Kolola <repoacc@protonmail.com>
2022-06-26 19:03:46 +0200

    SCP-1: Added; (c) MyAppState; Stateful components.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+
+class MyAppState extends State<MyApp>{
+
+  var questionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
 
   // This widget is the root of  app.
   @override
   Widget build(BuildContext context) {
+
+    var questions = [
+      'What\'s your fav color?',
+      'What\'s your fav animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title : Text('Score Pack')
-          ),
+        appBar: AppBar(
+          title : Text('Score Pack')
+        ),
         body: Column(
           children: [
-            Text('The question'),
+            Text(
+                questions[questionIndex],
+            ),
             ElevatedButton (
               child: Text('Answer1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             ElevatedButton (
-              child: Text('Answer2'),
-              onPressed: null,
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
             ),
             ElevatedButton (
-              child: Text('Answer3'),
-              onPressed: null,
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
             )
           ],
         ),
