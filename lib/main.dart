f8523832ae6141294b0f6f56fcabe132e200efa9
Alexei Kolola <repoacc@protonmail.com>
2022-06-29 21:16:20 +0200

    SCP-1: Added; (c) Quiz & Result; New (c)s.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:hackaburg/result.dart';
+import 'quiz.dart';
+import 'result.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +14,8 @@ class MyApp extends StatefulWidget {
   }
 }
 
-
-class _MyAppState extends State<MyApp>{
-  final questions = const [
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
     {
       'questionText': 'Whats your fav color?',
       'answers': ['Black', 'White', 'Red', 'Green'],
@@ -32,15 +32,14 @@ class _MyAppState extends State<MyApp>{
 
   var _questionIndex = 0;
 
-  void _answerQuestion(){
+  void _answerQuestion() {
     setState(() {
       _questionIndex++;
     });
     print(_questionIndex);
-    if(_questionIndex < questions.length){
+    if (_questionIndex < _questions.length) {
       print('We have more questions.');
-    }
-    else {
+    } else {
       print('No have more questions.');
     }
   }
@@ -49,23 +48,14 @@ class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title : Text('Score Pack')
-        ),
-        body: _questionIndex < questions.length ? Column(
-          children: [
-            Question(
-                questions[_questionIndex]['questionText'] as String
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
-                 return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ) : Center(
-          child: Text('You did it.'),
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(title: Text('Score Pack')),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              answerQuestion: _answerQuestion,
+              questionIndex: _questionIndex,
+              questions: _questions)
+          : Result(),
+    ));
   }
 }
