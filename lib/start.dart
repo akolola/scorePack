c63cac807353bb3f546d5259274778f84c0347ea
Alexei Kolola <repoacc@protonmail.com>
2022-07-02 20:03:52 +0200

    SCP-1: Added; (c) Start;  (c) Text -> (c) BoxDecoration.

--- lib/start.dart
+++ lib/start.dart
@@ -8,10 +8,21 @@ class Start extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Text(
-          'Choose new activity',
+        //=== 1. element:  description text.
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(3.0),
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+          child: const Text(
+            'Choose activity',
+            style: TextStyle(fontSize: 30.0),
+          ),
         ),
+
+        //=== 2. element: 2 navigation btns.
         ButtonBar(
           alignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
